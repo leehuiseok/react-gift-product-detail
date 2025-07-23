9ab41a73cbc2803a5b9fd9d88b85d53c65311435
leehuiseok <leehuiseok020412@gmail.com>
2025-07-23 21:17:46 +0900

    feat: Suspense ErrorBoundary 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Router } from '@/shared/Router';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ToastContainer } from 'react-toastify';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Suspense } from 'react';
+import { Loading } from '@/components/common/Loading';
+import { Error } from '@/components/common/Error';
 
 function App() {
   return (
     <AuthProvider>
-      <Router />
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<Loading />}>
+          <Router />
+        </Suspense>
+      </ErrorBoundary>
       <ToastContainer position="bottom-center" autoClose={1000} />
     </AuthProvider>
   );
